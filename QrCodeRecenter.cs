b73f37080e631cbd3247ff3663f96f0e48e4bc6b
Vedant Devre <96368377+Vedant2k03@users.noreply.github.com>
2023-01-30 01:15:16 +0530

    Add files via upload

---
+++ QrCodeRecenter.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using ZXing;
+
+public class QrCodeRecenter : MonoBehaviour
+{
+    [SerializeField]
+    private ARSession session;
+    [SerializeField]
+    private ARSessionOrigin sessionOrigin;
+    [SerializeField]
+    private ARCameraManager cameraManager;
+    [SerializeField]
+    private List<Target> navigationTargetObjects = new List<Target>();
+
+    private Texture2D cameraImageTexture;
+    private IBarcodeReader reader = new BarcodeReader();
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetQrCodeRecenterTarget("Physics Lab");
+        }
+    }
+
+    void OnEnable()
+    {
+        cameraManager.frameReceived += OnCameraFrameReceived;
+    }
+
+    void OnDisable()
+    {
+        cameraManager.frameReceived -= OnCameraFrameReceived;
+    }
+
+    private void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
+    {
+        if (!cameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
+        {
+            return;
+        }
+
+        var conversionParams = new XRCpuImage.ConversionParams
+        {
+            // Get the entire image
+            inputRect = new RectInt(0, 0, image.width, image.height),
+
+            // Downsample by 2
+            outputDimensions = new Vector2Int(image.width / 2, image.height / 2),
+
+            // Choose RGBA format
+            outputFormat = TextureFormat.RGBA32,
+
+            // Flip across the vertical axis (mirror image)
+            transformation = XRCpuImage.Transformation.MirrorY
+        };
+
+        // See how many bytes we need to store the final image.
+        int size = image.GetConvertedDataSize(conversionParams);
+
+        // Allocate a buffer to store the image
+        var buffer = new NativeArray<byte>(size, Allocator.Temp);
+
+        // Extract the image data
+        image.Convert(conversionParams, buffer);
+
+        // The image was converted to RGBA32 format and written into the provided buffer
+        // so we can dispose of the CameraImage. We must do this or it will leak resources.
+        image.Dispose();
+
+        // At this point, we could process the image, pass it to a computer vision algorithm, etc.
+        // In this example, we'll just apply it to a texture to visualize it.
+
+        // We've got the data; let's put it into a texture so we can visualize it.
+        cameraImageTexture = new Texture2D(
+            conversionParams.outputDimensions.x,
+            conversionParams.outputDimensions.y,
+            conversionParams.outputFormat,
+            false);
+
+        cameraImageTexture.LoadRawTextureData(buffer);
+        cameraImageTexture.Apply();
+
+        // Done with our temporary data
+        buffer.Dispose();
+
+        var result = reader.Decode(cameraImageTexture.GetPixels32(), cameraImageTexture.width, cameraImageTexture.height);
+
+        if (result != null)
+        {
+            SetQrCodeRecenterTarget(result.Text);
+        }
+    }
+
+    public void SetQrCodeRecenterTarget(string targetText)
+    {
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.ToLower().Equals(targetText.ToLower()));
+        if (currentTarget != null)
+        {
+            session.Reset();
+
+            sessionOrigin.transform.position = currentTarget.PositionObject.transform.position;
+            sessionOrigin.transform.rotation = currentTarget.PositionObject.transform.rotation;
+        }
+    }
+}
